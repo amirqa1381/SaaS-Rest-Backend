7bf11cb4047561688260f0b8821bd3f264c4646d
Dev <dev@example.com>
2026-08-30 17:30:32 +0330

    Refactor URL patterns to improve organization and readability; add email verification and password reset paths

--- src/apps/accounts/urls.py
+++ src/apps/accounts/urls.py
@@ -4,7 +4,12 @@ from rest_framework_simplejwt.views import (
     TokenBlacklistView,
 )
 from django.urls import path
-from apps.accounts.views import RegisterView
+from apps.accounts.views import (
+    RegisterView,
+    VerifyEmailView,
+    RequestPasswordResetView,
+    ResetPasswordView,
+)
 
 app_name = "accounts"
 
@@ -13,4 +18,15 @@ urlpatterns = [
     path("token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
     path("logout/", TokenBlacklistView.as_view(), name="token_blacklist"),
+    path("email/verify/", VerifyEmailView.as_view(), name="verify_email"),
+    path(
+        "request-password-reset/",
+        RequestPasswordResetView.as_view(),
+        name="request_password_reset",
+    ),
+    path(
+        "reset-password/<str:token>/",
+        ResetPasswordView.as_view(),
+        name=" reset_password",
+    ),
 ]
